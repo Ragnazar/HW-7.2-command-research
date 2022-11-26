1f831ca5191fe1ce3a94d17a662d18af92cebfb0
legio <ivan.avdeyev@wilix.org>
2022-11-26 14:53:27 +0300

    initial-commit

---
+++ src/main/java/pro/sky/command/TgBotApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.command;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgBotApplication.class, args);
+	}
+
+}
