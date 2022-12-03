f28000b2dc1732d012b763e0d4daa6b7e1b2974c
legio <ivan.avdeyev@wilix.org>
2022-12-03 13:25:22 +0300

    Javadoc для классов Владелец и Отчет

--- src/main/java/pro/sky/command/model/Owner.java
+++ src/main/java/pro/sky/command/model/Owner.java
@@ -6,9 +6,11 @@ import lombok.*;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
 
-import java.util.List;
-
-
+/**
+ * Класс владельцы питомца со свойствами <b>name</b> и <b>phone_number</b>.
+ * @autor Иван Авдеев
+ * @version 0.1
+ */
 @Entity
 @Table(name = "owner")
 @Getter
@@ -18,24 +20,29 @@ import java.util.List;
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Builder
 public class Owner {
+    /** Поле идентификатор владельца */
     @Id
     @GeneratedValue
     @Column(name = "id")
     @JdbcTypeCode(SqlTypes.BIGINT)
     private Long id;
 
+    /** Поле идентификатор чата с владельцем */
     @NotBlank
     @Column(name ="chat_id", nullable = false)
     private String chatId;
 
+    /** Поле имя владельца */
     @NotBlank
     @Column(nullable = false)
     private String name;
 
+    /** Поле телефонный номер владельца*/
     @NotBlank
     @Column(name = "phone_number", nullable = false)
     private String phoneNumber;
 
+    /** Поле идентификатор питомца из питомника */
     @NotBlank
     @OneToOne(optional = false)
     @JoinColumn(name = "pet_id", unique = true, nullable = false, updatable = false)
