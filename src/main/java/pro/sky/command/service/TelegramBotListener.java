c06db1128dcbbf1a00bacf272da8cd467f8bc609
NatShiv <happy_fox@bk.ru>
2022-12-09 01:55:21 +0300

    сделала пересылку сообщений в чат волонтерам. Исправила сущность владельца. добавила регистрацию владельца и выбор меню для зарегистрированных.

--- src/main/java/pro/sky/command/service/TelegramBotListener.java
+++ src/main/java/pro/sky/command/service/TelegramBotListener.java
@@ -3,30 +3,34 @@ package pro.sky.command.service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.send.SendDocument;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import pro.sky.command.configuration.TelegramBotConfiguration;
+import pro.sky.command.exception.ExecuteException;
 import pro.sky.command.service.handler.HandlerCallbackQuery;
 import pro.sky.command.service.handler.HandlerCommand;
+import pro.sky.command.service.handler.HandlerMessages;
+
+import java.util.List;
 
 
 @Component
 @Slf4j
 public class TelegramBotListener extends TelegramLongPollingBot {
-
-
     private final TelegramBotConfiguration configuration;
     private final HandlerCallbackQuery callbackQuery;
     private final HandlerCommand handlerCommand;
+    private final HandlerMessages handlerMessages;
 
-    public TelegramBotListener(TelegramBotConfiguration configuration, HandlerCallbackQuery callbackQuery, HandlerCommand handlerCommand) {
+    public TelegramBotListener(TelegramBotConfiguration configuration, HandlerCallbackQuery callbackQuery, HandlerCommand handlerCommand, HandlerMessages handlerMessages) {
         this.configuration = configuration;
         this.callbackQuery = callbackQuery;
         this.handlerCommand = handlerCommand;
-
+        this.handlerMessages = handlerMessages;
     }
 
     @Override
@@ -45,27 +49,32 @@ public class TelegramBotListener extends TelegramLongPollingBot {
         if (update.hasMessage()) {
             Message message = update.getMessage();
             if (message.hasText() && message.hasEntities()) {
-               executeMessage(handlerCommand.handleMessage(message));
+                executeMessage(handlerCommand.handleMessage(message));
+            } else if (message.hasText()) {
+                executeMessage(handlerMessages.handleText(message));
             }
-//вставить обработчик сообщений
         } else if (update.hasCallbackQuery()) {
-            executeEditText(callbackQuery.handleCallbackQuery(update));
+            executeMessage(callbackQuery.handleCallbackQuery(update));
         }
     }
 
-    private void executeMessage(SendMessage sendMessage) {
+    private void executeMessage(Object o) {
         try {
-            execute(sendMessage);
-        } catch (TelegramApiException e) {
-            log.error("Возникла ошибка при отправке сообщения в телеграм");
-        }
-    }
-
-    private void executeEditText(EditMessageText sendMessage) {
-        try {
-            execute(sendMessage);
-        } catch (TelegramApiException e) {
-            log.error("Возникла ошибка при отправке сообщения в телеграм");
+            if (o instanceof BotApiMethod<?>) {
+                execute((BotApiMethod<?>) o);
+            } else if (o instanceof SendPhoto) {
+                execute((SendPhoto) o);
+            } else if (o instanceof SendDocument) {
+                execute((SendDocument) o);
+            } else if (o instanceof List<?>) {
+                for (Object m : (List<?>) o) {
+                    execute((BotApiMethod<?>) m);
+                }
+            } else {
+                throw new ExecuteException("Что то пошло не так при отправке сообщения. Возможно передан объект не поддерживаемый методом");
+            }
+        } catch (ExecuteException | TelegramApiException e) {
+            log.error("Возникла ошибка при отправке сообщения в телеграм" + e.getMessage());
         }
     }
 }
--- src/main/resources/db/changelog/scipts/tasks.sql
+++ src/main/resources/db/changelog/scipts/tasks.sql
@@ -1,10 +1,23 @@
 -- liquibase formatted sql
 
 -- changeset iavdeyev:1
-CREATE TABLE owner (
-                                   id                SERIAL,
-                                   chat_id           TEXT,
-                                   name              TEXT,
-                                   phone_number      TEXT,
-                                   pet_id            BIGINT
+
+CREATE TABLE Owner
+(
+    chatId        VARCHAR(255) NOT NULL,
+    name          VARCHAR(255) NOT NULL,
+    phone_number  VARCHAR(255),
+    shelterButton VARCHAR(255),
+    volunteerChat BOOLEAN      NOT NULL,
+    pet_id        BIGINT,
+    CONSTRAINT pk_owner PRIMARY KEY (chatId)
+);
+
+-- changeset NataliShilova:1
+CREATE TABLE Notification
+(
+    id        BIGINT  NOT NULL,
+    text      VARCHAR(255)NOT NULL,
+    messageId INTEGER NOT NULL,
+    CONSTRAINT pk_notification PRIMARY KEY (id)
 );
