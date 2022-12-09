65a84f24aca7f207fae73d47b7ced7d63e05d48d
NatShiv <happy_fox@bk.ru>
2022-12-09 09:39:08 +0300

    исправила тип поля

--- src/main/resources/db/changelog/scipts/tasks.sql
+++ src/main/resources/db/changelog/scipts/tasks.sql
@@ -4,8 +4,8 @@
 
 CREATE TABLE Owner
 (
-    chatId        TEXT NOT NULL,
-    name          TEXT NOT NULL,
+    chatId        TEXT    NOT NULL,
+    name          TEXT    NOT NULL,
     phone_number  TEXT,
     shelterButton TEXT,
     volunteerChat BOOLEAN NOT NULL,
@@ -14,29 +14,31 @@ CREATE TABLE Owner
 );
 
 -- changeset dlukin:2
-CREATE TABLE pets (
-    id           SERIAL,
-    name_pet         TEXT,
-    owner_id       	BIGINT,
-    kind            TEXT,
-    report_id       BIGINT
+CREATE TABLE pets
+(
+    id        SERIAL,
+    name_pet  TEXT,
+    owner_id  BIGINT,
+    kind      TEXT,
+    report_id BIGINT
 );
 
 -- changeset iavdeyev:3
-CREATE TABLE report (
-                       id                SERIAL,
-                       recording_date    TIMESTAMP,
-                       photo             BYTEA,
-                       diet              TEXT,
-                       state_of_health   TEXT,
-                       behavior_changes  TEXT
-                       
+CREATE TABLE report
+(
+    id               SERIAL,
+    recording_date   TIMESTAMP,
+    photo            BYTEA,
+    diet             TEXT,
+    state_of_health  TEXT,
+    behavior_changes TEXT
+
 );
 -- changeset NataliShilova:4
 CREATE TABLE Notification
 (
     id        BIGINT  NOT NULL,
-    text      VARCHAR(255)NOT NULL,
+    text      TEXT    NOT NULL,
     messageId INTEGER NOT NULL,
     CONSTRAINT pk_notification PRIMARY KEY (id)
 );
