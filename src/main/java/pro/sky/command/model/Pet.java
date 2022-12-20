32409cedfb74d8e02525a9fa0b05b63664916329
legio <ivan.avdeyev@wilix.org>
2022-12-20 20:48:06 +0300

    small fixes

--- src/main/java/pro/sky/command/model/Pet.java
+++ src/main/java/pro/sky/command/model/Pet.java
@@ -1,44 +1,53 @@
 package pro.sky.command.model;
 
-import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
+
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
+import org.hibernate.annotations.Table;
+
+import javax.persistence.*;
 import java.util.List;
 
 @Entity
-@Table(name = "pets")
+@Table(appliesTo = "pet")
 @Getter
 @Setter
-@ToString
-@NoArgsConstructor
 public class Pet {
     /**
      * Поле идентификатор питомца
      */
-    @Id
-    @NotBlank
+    @javax.persistence.Id
     private Long id;
     /**
      * Поле имя питомца
      */
     @Column(name = "name_pet")
-    @NotBlank
     private String namePet;
 
-    /** Поле идентификатор принадлежности к определенному владельцу */
+    /**
+     * Поле идентификатор принадлежности к определенному владельцу
+     */
     @JoinColumn(name = "owner_id")
     @ManyToOne(optional = false)
     private Owner owner;
-    /** Поле вид питомца (кошка, собака) */
-    @NotBlank
+    /**
+     * Поле вид питомца (кошка, собака)
+     */
     @Column(name = "kind")
     private String kindOfAnimal;
-    /** Поле идентификатор идентификатор отчета о питомце из таблицы report */
+    /**
+     * Поле идентификатор идентификатор отчета о питомце из таблицы report
+     */
 
     @OneToMany(mappedBy = "pet")
     private List<Report> reports;
 
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "(" +
+                "id = " + id + ", " +
+                "namePet = " + namePet + ", " +
+                "owner = " + owner + ", " +
+                "kindOfAnimal = " + kindOfAnimal + ")";
+    }
 }
