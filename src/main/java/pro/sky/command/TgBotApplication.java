c9819eae43ce3447035f580a18cbfc543bc08878
legio <ivan.avdeyev@wilix.org>
2022-12-20 20:00:38 +0300

    small fixes

--- src/main/java/pro/sky/command/TgBotApplication.java
+++ src/main/java/pro/sky/command/TgBotApplication.java
@@ -1,12 +1,11 @@
 package pro.sky.command;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition(servers = {@Server(url = "http://127.0.0.1:8081")})
+@OpenAPIDefinition
 public class TgBotApplication {
 
 	public static void main(String[] args) {
